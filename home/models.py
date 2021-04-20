085798a7155a7d8fb1347014d963e7b860330f34
callumhearne <callum.hearne98@gmail.com>
2021-04-20 22:15:25 +0000

    i have customised the photo views for admin level

--- home/models.py
+++ home/models.py
@@ -5,7 +5,11 @@ from django.db import models
 
 class Photos(models.Model):
 
+    class meta:
+        verbaose_name = 'Photos'
+
     Location = models.CharField(max_length=100)
     desc = models.TextField()
     price = models.IntegerField()
     img = models.ImageField(upload_to='media')
+    NE = models.BooleanField(default=True)
