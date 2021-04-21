4522162e8ead66225fbccf1751f8a970614d874d
callumhearne <callum.hearne98@gmail.com>
2021-04-21 21:21:20 +0000

    i have sorted all of the filters for the nav bar and on the photos page itself

--- photos/urls.py
+++ photos/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.photos, name='photos')
+    path('', views.photos, name='photos'),
+    path('<photo_id>', views.photo_detail, name='photo_detail')
 
 
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Photo
 
 # Create your views here.
@@ -8,21 +11,52 @@ def photos(request):
     """ A view to return all photos page including the user searching """
 
     photos = Photo.objects.all()
-    print(photos)
+    query = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                photos = photos.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            photos = photos.order_by(sortkey)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('photos'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            photos = photos.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
        'photos': photos,
+       'search_term': query,
+       'current_sorting': current_sorting,
+
     }
 
     return render(request, 'photos/photos.html', context)
 
 
-def photos_detail(request, photo_id):
-    """ A view to show certain photo informartion"""
+def photo_detail(request, photo_id):
+    """ A view to show certain photo information"""
 
-    photos = get_object_or_404(Photo, pk=photo_id)
+    photo = get_object_or_404(Photo, pk=photo_id)
 
     context = {
-       'photos': photos,
+       'photo': photo,
     }
 
-    return render(request, 'photos/photos_detail.html', context)
+    return render(request, 'photos/photo_detail.html', context)
