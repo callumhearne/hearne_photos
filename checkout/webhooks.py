809b6fe44fb1439f73875d2a3af63b74fe665a48
callumhearne <callum.hearne98@gmail.com>
2021-04-23 14:07:57 +0000

    I have completed all of the stripe and webhook logic so all payments being completed will show up in stripe as succesful working exactally as intended

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -34,9 +34,6 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    print('Success!')
-    return HttpResponse(status=200)
-   
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
@@ -56,4 +53,3 @@ def webhook(request):
     # Call the event handler with the event
     response = event_handler(event)
     return response
-
