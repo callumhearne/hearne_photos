067716a06a519020a8f79b3c00a1f178bfe7aa35
callumhearne <callum.hearne98@gmail.com>
2021-04-25 11:43:13 +0000

    ive removed the secret key from settings.py

--- hearne_photos/settings.py
+++ hearne_photos/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5h$876(e&zt9b0_zz(enl!4j$%=s*cyd_b59szq_6ib#ux%m$)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['hearne-photos.herokuapp.com', 'localhost']
 
