cb8343410000466b737aad3463b7e8bb450b84c9
callumhearne <callum.hearne98@gmail.com>
2021-04-26 08:29:34 +0000

    noticed the stripe secret key was wrong so thats corrected

--- hearne_photos/settings.py
+++ hearne_photos/settings.py
@@ -209,7 +209,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
