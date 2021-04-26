eaccde8d2122e1e3aa6857599f5bbfed96b5d0b3
callumhearne <callum.hearne98@gmail.com>
2021-04-26 14:57:27 +0000

    ive added refund and faq pages to the website that can be accessed through the profile tab

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('faq/', views.faq, name='faq'),
+    path('policy/', views.policy, name='policy'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,13 @@ def index(request):
     """ A command to return the home page """
 
     return render(request, 'home/index.html')
+
+def faq(request):
+    """ A command to return the home page """
+
+    return render(request, 'home/faq.html')
+
+def policy(request):
+    """ A command to return the home page """
+
+    return render(request, 'home/policy.html')
