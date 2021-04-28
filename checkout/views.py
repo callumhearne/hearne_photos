006ab21b5ec6a1c7df82f52085b257d750e1ae24
callumhearne <callum.hearne98@gmail.com>
2021-04-28 23:43:59 +0000

    correcting an error with checkout success

--- checkout/views.py
+++ checkout/views.py
@@ -71,7 +71,7 @@ def checkout(request):
                         order_line_item.save()
                     else:
                         for size, quantity in (
-                                        item_data['items_by_size'].items()):
+                            item_data['items_by_size'].items()):
                             order_line_item = OrderLineItem(
                                 order=order,
                                 photo=photo,
